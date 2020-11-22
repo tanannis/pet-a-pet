3eb2a8c71af09081eb8ef582549eb1d90b1ca554
tanannis <tanannis@gmail.com>
2020-11-22 14:29:54 -0500

    SinglePet: added ScrollView

--- components/SinglePet.js
+++ components/SinglePet.js
@@ -8,8 +8,12 @@ import {
 	Image,
 	TextInput,
 	Alert,
+	ScrollView,
+	ImageBackground,
 } from "react-native";
-import { dogs } from "../db";
+import Constants from 'expo-constants'
+import { dogs } from "../db"
+
 
 //render the info of a single pet
 //dogs = [{..},{..},{..}]
@@ -26,10 +30,18 @@ class SinglePet extends React.Component {
 			//if dog.id is the selected dog id
 			if (dog.id === this.props.route.params.dog.id)
 				return (
-					<View style={styles.image}>
-						<Text>{dog.name}</Text>
-						<Image style={styles.image} source={dog.uri} />
-					</View>
+						<View style={{flex: 1}} >
+							<ImageBackground style={styles.image} source={dog.uri}> 
+							<Text style={styles.nameText}>{dog.name}</Text>
+							</ImageBackground>
+							<SafeAreaView style={styles.container}>
+							<ScrollView  style={styles.scrollView} >
+							<Text style={styles.text}>My name is kobe, I'm a good body, I love mommy and daddy, want to be friends with me? message me below ^^</Text>
+							</ScrollView>
+							</SafeAreaView>
+						</View>
+		
+			
 				);
 		}
 	}
@@ -37,11 +49,32 @@ class SinglePet extends React.Component {
 
 const styles = StyleSheet.create({
 	image: {
-        flex: 1,
-        height: null,
-        width: null,
-        resizeMode: 'contain',
+		flex: 2,
+		height: null,
+		width: null,
+		resizeMode: "cover",
 	},
+	container: {
+		flex: 1,
+		marginTop: Constants.statusBarHeight
+	},
+	contentContainer: {
+		flex: 1,
+		paddingVertical: 20
+	},
+	scrollView:{
+		// backgroundColor: 'pink',
+		marginHorizontal: 20,
+	},
+	text: {
+		fontSize: 18
+	},
+	nameText: {
+		fontWeight: 'bold',
+		color: 'blue',
+		fontSize: 28
+	}
+	
 });
 
 export default SinglePet;
